20d344141c3ba373a10e667c12cd4e207faa5118
w-phil <wphillips892@gmail.com>
2022-03-06 10:57:15 -0500

    deployment

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 8000
 
 require('./server/config/mongoose.config');
 
@@ -20,8 +21,12 @@ require('./server/routes/login.routes')(app);
 require('./server/routes/user.routes')(app);
 require('./server/routes/photo.routes')(app);
 
-app.listen(8000, () => {
-    console.log("Listening at port 8000");
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`);
 })
 
 
