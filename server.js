afd69d8e860e9bb7fccf88ec2b808c0fd03754d3
w-phil <wphillips892@gmail.com>
2022-03-07 07:36:12 -0500

    deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 require('./server/config/mongoose.config');
 
