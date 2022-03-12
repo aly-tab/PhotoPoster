d8190ed324d5ad03ba98441c2832e4cd253f61c7
w-phil <wphillips892@gmail.com>
2022-03-12 09:46:20 -0500

    deployment

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ require('./server/routes/photo.routes')(app);
 
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'));
+}
+
 app.listen(port, () => {
     console.log(`Listening at port ${port}`);
 })
